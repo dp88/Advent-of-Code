57f96898dc06f8bf4c6a5fc5dff2510c176b365c
DP <dp@reproconnect.com>
2020-12-03 20:43:23 -0500

    day 1 part 2

--- 2020/day01/solution.js
+++ 2020/day01/solution.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync( __dirname + '/input.txt', 'utf8').split('\r\n');
 let a = 0,
     b = 0;
 
-let multiple;
+let multipleA;
 
 (() => {
     for (a = 0; a < input.length; a++) {
@@ -15,7 +15,7 @@ let multiple;
                   numB = parseInt(input[b]);
 
             if (numA + numB == 2020) {
-                multiple = numA * numB;
+                multipleA = numA * numB;
 
                 console.log('solution part 1)');
                 console.log(`first number (${a}) is ${numA}`);
@@ -28,4 +28,40 @@ let multiple;
     }
 })();
 
-console.log(`solution: ${multiple}`);
+console.log(`solution: ${multipleA}`);
+
+let x = 0,
+    y = 0,
+    z = 0;
+
+let multipleB;
+
+(() => {
+    for (x = 0; x < input.length; x++) {
+        for (y = 0; y < input.length; y++) {
+            if (x == y) continue;
+
+            for (z = 0; z < input.length; z++) {
+                if (x == z || y == z) continue;
+
+                const numX = parseInt(input[x]),
+                      numY = parseInt(input[y]),
+                      numZ = parseInt(input[z])
+
+                if (numX + numY + numZ == 2020) {
+                    multipleB = numX * numY * numZ;
+
+                    console.log('solution part 2)');
+                    console.log(`first number (${x}) is ${numX}`);
+                    console.log(`second number (${y}) is ${numY}`);
+                    console.log(`second number (${z}) is ${numZ}`);
+                    console.log(`${numX} + ${numY} + ${numZ} = 2020? ${numX + numY + numZ == 2020 ? 'yes' : 'no'}`);
+
+                    return;
+                }
+            }
+        }
+    }
+})();
+
+console.log(`solution: ${multipleB}`);
