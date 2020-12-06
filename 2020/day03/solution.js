b4c0a1dd29e2145c3fff19a813f60f8c991998d7
DP <dp@reproconnect.com>
2020-12-05 22:05:50 -0500

    day 3 part 2

--- 2020/day03/solution.js
+++ 2020/day03/solution.js
@@ -2,14 +2,28 @@ const fs = require('fs');
 const input = fs.readFileSync( __dirname + '/input.txt', 'utf8').split('\r\n');
 
 const width = input[0].length;
-let trees = 0;
 
-let column = 3;
-for (let row = 1; row < input.length; row++) {
-    if (input[row][column % width] == '#') trees++;
+const countPath = function countPath (right, down) {
+    let trees = 0;
 
-    column += 3;
+    let column = right;
+    for (let row = down; row < input.length; row += down) {
+        if (input[row][column % width] == '#') trees++;
+
+        column += right;
+    }
+
+    return trees;
 }
 
 console.log('solution part 1)');
-console.log(`trees: ${trees}`);
+console.log(`trees: ${countPath(3, 1)}`);
+
+const part2 = countPath(1, 1) *
+              countPath(3, 1) *
+              countPath(5, 1) *
+              countPath(7, 1) *
+              countPath(1, 2);
+
+console.log('solution part 2)');
+console.log(`trees: ${part2}`);
