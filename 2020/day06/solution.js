c5acc36accdcd6178966a697ff3f3bbf4664c10a
DP <dp@reproconnect.com>
2020-12-08 21:59:24 -0500

    day 6 part 2

--- 2020/day06/solution.js
+++ 2020/day06/solution.js
@@ -1,20 +1,36 @@
 const fs = require('fs');
 const input = fs.readFileSync( __dirname + '/input.txt', 'utf8').split('\r\n\r\n');
 
-let sum = 0;
+let anyYesSum = 0;
+let consensusYesSum = 0;
+
 for (const group of input) {
-    let answers = '';
+    let answers = {};
+    let people = 0;
 
     for (const person of group.split('\r\n')) {
+        people++;
+
         for (const answer of person) {
-            if (answers.indexOf(answer) == -1) {
-                answers += answer;
+            if (typeof answers[answer] == 'undefined') {
+                answers[answer] = 0;
             }
+
+            answers[answer]++;
         }
     }
 
-    sum += answers.length;
+    anyYesSum += Object.keys(answers).length;
+    
+    for (const i in answers) {
+        if (answers[i] == people) {
+            consensusYesSum += 1;
+        }
+    }
 }
 
 console.log('solution part 1)');
-console.log(`total yes: ${sum}`);
+console.log(`total yes: ${anyYesSum}`);
+
+console.log('solution part 2)');
+console.log(`consensus yes: ${consensusYesSum}`);
