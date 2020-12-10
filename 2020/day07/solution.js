4033ddc18a885b3b3ce3de4a67cfc2b38d5abe7e
DP <dp@reproconnect.com>
2020-12-09 21:35:54 -0500

    day 7 part 2

--- 2020/day07/solution.js
+++ 2020/day07/solution.js
@@ -7,6 +7,7 @@ class Bag {
     constructor(name) {
         this.name = name;
         this.contains = {};
+        this.containsQty = {};
         this.containedIn = {};
     }
 
@@ -26,6 +27,31 @@ class Bag {
 
         return parents.filter((value, index, self) => self.indexOf(value) === index);
     }
+
+    getChildren() {
+        const children = {};
+
+        for (const c in this.containsQty) {
+            if (typeof children[c] == 'undefined') {
+                children[c] = 0;
+            }
+
+            children[c] += this.containsQty[c];
+
+            const grandchildren = this.contains[c].getChildren();
+            const multiple = this.containsQty[c];
+
+            for (const gc in grandchildren) {
+                if (typeof children[gc] == 'undefined') {
+                    children[gc] = 0;
+                }
+
+                children[gc] += grandchildren[gc] * multiple;
+            }
+        }
+
+        return children;
+    }
 }
 
 for (const rule of input) {
@@ -33,12 +59,20 @@ for (const rule of input) {
     const container = Bag.getOrCreate(split[0]);
 
     for (const bag of split[1].split(', ')) {
-        const containee = Bag.getOrCreate(bag.replace(/[0-9\.]|(bags|bag)/g, '').trim());
+        const name = bag.replace(/[0-9\.]|(bags|bag)/g, '').trim();
+        if (name == 'no other') continue;
 
+        const containee = Bag.getOrCreate(name);
         containee.containedIn[container.name] = container;
         container.contains[containee.name] = containee;
+        container.containsQty[containee.name] = parseInt(bag);
     }
 }
 
-console.log('solution part 1)')
+console.log('solution part 1)');
 console.log(`shiny gold container possibilities: ${Bag.all['shiny gold'].getParents().length - 1}`);
+
+let shinyGoldChildren = 0;
+Object.values(Bag.all['shiny gold'].getChildren()).forEach((qty) => shinyGoldChildren += qty);
+console.log('solution part 2)');
+console.log(`a shiny gold bag contains ${shinyGoldChildren} other bags... oof`);
