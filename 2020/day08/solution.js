01113326e95614192ae033e86ccc994d183303d0
DP <dp@reproconnect.com>
2020-12-11 22:04:05 -0500

    day 8 part 2

--- 2020/day08/solution.js
+++ 2020/day08/solution.js
@@ -1,26 +1,61 @@
 const fs = require('fs');
 const input = fs.readFileSync( __dirname + '/input.txt', 'utf8').split('\r\n');
 
-let instructions = [];
-let accumulator = 0;
-let accumulatorPrev = 0;
-const computer = {
-    'acc': (arg) => {
-        accumulator += arg;
-        return 1;
-    },
-    'jmp': (arg) => arg,
-    'nop': (arg) => 1,
-};
+class Computer {
+    operation = {
+        'acc': (arg) => {
+            this.accumulator.current += arg;
+            return 1;
+        },
+        'jmp': (arg) => arg,
+        'nop': (arg) => 1,
+    };
 
-let i = 0;
-while (instructions.length == [...(new Set(instructions))].length) {
-    const instruction = input[i].split(' ');
+    constructor () {
+        this.instructions = [];
+        this.accumulator = {
+            current: 0,
+            previous: 0,
+        };
+    }
 
-    instructions.push(i);
-    accumulatorPrev = accumulator;
-    i += computer[instruction[0]](parseInt(instruction[1]));
+    execute (switchInstruction = -1) {
+        let i = 0;
+        while (this.instructions.length == [...(new Set(this.instructions))].length) {
+            const instruction = input[i].split(' ');
+
+            if (switchInstruction == i) {
+                if (instruction[0] == 'jmp') {
+                    instruction[0] = 'nop';
+                } else if (instruction[0] == 'nop') {
+                    instruction[0] = 'jmp';
+                }
+            }
+    
+            this.instructions.push(i);
+            this.accumulator.previous = this.accumulator.current;
+            i += this.operation[instruction[0]](parseInt(instruction[1]));
+
+            if (i >= input.length) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
+const computer = new Computer();
+computer.execute();
+
 console.log('solution part 1)');
-console.log(`last valid accumulator value: ${accumulatorPrev}`);
+console.log(`last valid accumulator value: ${computer.accumulator.previous}`);
+
+console.log('solution part 2)');
+for (let i = 0; i < input.length; i++) {
+    const computer = new Computer();
+    if (computer.execute(i)) {
+        console.log(`accumulator result: ${computer.accumulator.current}`);
+        break;
+    }
+}
