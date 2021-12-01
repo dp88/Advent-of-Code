d348a27977fa313eb54408a779325fe15677975e
DP <dp@reproconnect.com>
2021-12-01 09:55:07 -0500

    day 1 part 2

--- 2021/day01/solution.js
+++ 2021/day01/solution.js
@@ -1,12 +1,32 @@
 const fs = require('fs');
 const input = fs.readFileSync( __dirname + '/input.txt', 'utf8').split('\r\n');
 
-let increases = 0;
-for (let i = 1; i < input.length; i++) {
-    if (parseInt(input[i]) > parseInt(input[i - 1])) {
-        increases++;
+function measureWindow(start, windowSize = 1) {
+    let sum = 0;
+
+    for (let i = 0; i < windowSize; i++) {
+        sum += parseInt(input[start + i]);
     }
+
+    return sum / windowSize;
+}
+
+function countIncreases(windowSize = 1) {
+    let increases = 0;
+
+    for (let i = 0; i < input.length - windowSize; i++) {
+        if (measureWindow(i + 1, windowSize) > measureWindow(i, windowSize)) {
+            increases++;
+        }
+    }
+
+    return increases;
 }
 
 console.log('solution part 1)');
-console.log(`number of increases: ${increases}`);
+console.log(`number of increases: ${countIncreases()}`);
+
+console.log('----------------------------------------------------------------------');
+
+console.log('solution part 2)');
+console.log(`averaged number of increases: ${countIncreases(3)}`);
