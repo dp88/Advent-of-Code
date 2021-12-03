f01d61b8854fb9ec950d8c66fbddd0e55460c1dc
DP <dp@reproconnect.com>
2021-12-02 20:53:12 -0500

    day 2 part 2

--- 2021/day02/solution.js
+++ 2021/day02/solution.js
@@ -1,19 +1,38 @@
 const fs = require('fs');
 const input = fs.readFileSync( __dirname + '/input.txt', 'utf8').split('\r\n');
 
-let horizontal = 0,
-    depth = 0;
+const dumbEngine = {
+    'horizontal': 0,
+    'depth': 0,
+    'forward': (value) => dumbEngine.horizontal += value,
+    'up': (value) => dumbEngine.depth -= value,
+    'down': (value) => dumbEngine.depth += value,
+    'product': () => dumbEngine.horizontal * dumbEngine.depth,
+};
 
-const engine = {
-    'forward': (value) => horizontal += value,
-    'up': (value) => depth -= value,
-    'down': (value) => depth += value,
+const smartEngine = {
+    'horizontal': 0,
+    'depth': 0,
+    'aim': 0,
+    'forward': (value) => {
+        smartEngine.horizontal += value;
+        smartEngine.depth += (smartEngine.aim * value);
+    },
+    'up': (value) => smartEngine.aim -= value,
+    'down': (value) => smartEngine.aim += value,
+    'product': () => smartEngine.horizontal * smartEngine.depth,
 };
 
 for (let i = 0; i < input.length; i++) {
     const [command, value] = input[i].split(' ');
-    engine[command](parseInt(value));
+    dumbEngine[command](parseInt(value));
+    smartEngine[command](parseInt(value));
 }
 
 console.log('solution part 1)');
-console.log(`distance product: ${horizontal * depth}`);
+console.log(`distance product: ${dumbEngine.product()}`);
+
+console.log('----------------------------------------------------------------------');
+
+console.log('solution part 2)');
+console.log(`distance product: ${smartEngine.product()}`);
