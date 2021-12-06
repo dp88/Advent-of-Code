ee61fb1c9e38ae1d7437c9bd3b4401122e462f00
DP <dp@reproconnect.com>
2021-12-05 21:21:31 -0500

    day 5 part 2

--- 2021/day05/solution.js
+++ 2021/day05/solution.js
@@ -1,7 +1,32 @@
 const fs = require('fs');
 const input = fs.readFileSync( __dirname + '/input.txt', 'utf8').split('\r\n');
 
-const map = {};
+const straightMap = {},
+      fullMap = {};
+
+function range(n, o) {
+    let result = [n],
+        current = n;
+
+    while (current != o) {
+        if (n > o) current--;
+        if (n < o) current++;
+        result.push(current);
+    }
+
+    return result;
+}
+
+function plot(a, b, map) {
+    const x = range(a[0], b[0]),
+          y = range(a[1], b[1]),
+          safe = (array, index) => index < array.length ? array[index] : array[0];
+
+    for (let i = 0; i < Math.max(x.length, y.length); i++) {
+        const xy = `${safe(x, i)},${safe(y, i)}`;
+        map[xy] = typeof map[xy] == 'number' ? map[xy] + 1 : 1;
+    }
+}
 
 for (let i = 0; i < input.length; i++) {
     let [a, b] = input[i]
@@ -10,18 +35,17 @@ for (let i = 0; i < input.length; i++) {
             .split(',')
             .map((xy) => parseInt(xy)));
     
-    if (a[0] == b[0]) {
-        for (let y = Math.min(a[1], b[1]); y < Math.max(a[1], b[1]) + 1; y++) {
-            const xy = `${a[0]},${y}`;
-            map[xy] = typeof map[xy] == 'number' ? map[xy] + 1 : 1;
-        }
-    } else if (a[1] == b[1]) {
-        for (let x = Math.min(a[0], b[0]); x < Math.max(a[0], b[0]) + 1; x++) {
-            const xy = `${x},${a[1]}`;
-            map[xy] = typeof map[xy] == 'number' ? map[xy] + 1 : 1;
-        }
+    if (a[0] == b[0] || a[1] == b[1]) {
+        plot(a, b, straightMap);
     }
+
+    plot(a, b, fullMap);
 }
 
 console.log('solution part 1)');
-console.log(`dangerous intersections: ${Object.values(map).filter((value) => value > 1).length}`);
+console.log(`dangerous intersections: ${Object.values(straightMap).filter((value) => value > 1).length}`);
+
+console.log('----------------------------------------------------------------------');
+
+console.log('solution part 2)');
+console.log(`super dangerous intersections: ${Object.values(fullMap).filter((value) => value > 1).length}`);
