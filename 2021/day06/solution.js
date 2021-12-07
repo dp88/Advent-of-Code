65eb9a69168341bece977a7312f72df776011c15
DP <dp@reproconnect.com>
2021-12-07 08:51:30 -0500

    day 6 part 2

--- 2021/day06/solution.js
+++ 2021/day06/solution.js
@@ -1,23 +1,25 @@
 const fs = require('fs');
-let fish = fs.readFileSync( __dirname + '/input.txt', 'utf8').split(',');
+const input = fs.readFileSync( __dirname + '/input.txt', 'utf8').split(',');
 
-let BBQ = 0;
+let BBQ = [0,0];
+const adults = [0,0,0,0,0,0,0];
 
-for (let i = 0; i < 80; i++) {
-    fish = fish.map((f) => {
-        if (f == 0) {
-            BBQ++;
-            return 6;
-        }
+for (const fish of input) {
+    adults[fish]++;
+}
 
-        return f - 1;
-    });
+for (let i = 0; i < 256; i++) {
+    const births = adults[i % 7];
+    adults[i % 7] += BBQ[i % 2];
+    BBQ[i % 2] = births;
 
-    while (BBQ > 0) {
-        fish.push(8);
-        BBQ--;
+    if (i == 79) {
+        console.log('solution part 1)');
+        console.log(`fish after 80 days: ${adults.reduce((a, b) => a + b) + BBQ[0] + BBQ[1]}`);
+
+        console.log('----------------------------------------------------------------------');
     }
 }
 
-console.log('solution part 1)');
-console.log(`fish after 80 days: ${fish.length}`);
+console.log('solution part 2)');
+console.log(`fish after 256 days: ${adults.reduce((a, b) => a + b) + BBQ[0] + BBQ[1]}`);
