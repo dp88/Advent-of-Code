a14b293ddaa0573bcf7e67a3d74fc19d3963296d
DP <dp@reproconnect.com>
2021-12-09 14:07:02 -0500

    day 9 part 2

--- 2021/day09/solution.js
+++ 2021/day09/solution.js
@@ -10,29 +10,62 @@ for (const line of input) {
     }
 }
 
-function safeAccess(x, y) {
-    if (x < 0 || x >= map.length) return 9;
-    if (y < 0 || y >= input.length) return 9;
+const IndexToXY = (i) => [ i % width, Math.floor(i / width) ];
 
-    const i = (y * width) + x;
-    return i >= 0 && i < map.length ? map[i] : 10;
+function safeIndex(x, y) {
+    if (x < 0 || x >= input[0].length) return -1;
+    if (y < 0 || y >= input.length) return -1;
+
+    return (y * width) + x;
 }
 
+const neighbors = (x, y) => [
+    safeIndex(x - 1, y), // West
+    safeIndex(x + 1, y), // East
+    safeIndex(x, y - 1), // North
+    safeIndex(x, y + 1), // South
+].filter((i) => i >= 0);
+
+let lowPoints = [];
 let riskSum = 0;
 
 for (let i = 0; i < map.length; i++) {
     if (map[i] == 9) continue;
 
-    const x = i % width,
-          y = Math.floor(i / width);
-
-    if (safeAccess(x - 1, y) <= map[i]) continue; // West
-    if (safeAccess(x + 1, y) <= map[i]) continue; // East
-    if (safeAccess(x, y - 1) <= map[i]) continue; // North
-    if (safeAccess(x, y + 1) <= map[i]) continue; // South
+    const [x, y] = IndexToXY(i);
+    if (neighbors(x, y).filter((n) => map[n] <= map[i]).length > 0) continue;
 
     riskSum += 1 + map[i];
+    lowPoints.push(i);
 }
 
 console.log('solution part 1)');
 console.log(`total risk: ${riskSum}`);
+
+console.log('----------------------------------------------------------------------');
+
+let basins = [];
+
+for (const point of lowPoints) {
+    let open = [point],
+        found = [];
+
+    while (open.length > 0) {
+        const current = open[0];
+        const [x, y] = IndexToXY(current);
+        open = open.slice(1);
+        found.push(current);
+
+        open.push(...neighbors(x, y)
+            .filter((n) => !open.includes(n) && !found.includes(n))
+            .filter((n) => map[n] < 9)
+        );
+    }
+
+    basins.push(found.length);
+}
+
+basins = basins.sort((a, b) => b - a).slice(0, 3);
+
+console.log('solution part 2)');
+console.log(`product of largest basins ${basins}: ${basins.reduce((a, b) => a * b)}`);
