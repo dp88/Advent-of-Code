f8f1db5eec20bc3f2629dc3f7cbfe11a3dd2d5e5
DP <dp@reproconnect.com>
2022-12-02 20:43:20 -0500

    day 1 part 2

--- 2022/day01/solution.ts
+++ 2022/day01/solution.ts
@@ -6,9 +6,15 @@ const backpacks = input.split('\r\n\r\n')
         return contents.map((str) => parseInt(str));
     });
 
-const largest = backpacks
+const sorted = backpacks
     .map((pack) => pack.reduce((sum, food) => sum + food, 0))
-    .sort((a, b) => b - a)[0];
+    .sort((a, b) => b - a)
+    .slice(0, 3);
 
 console.log('solution part 1)');
-console.log(`highest caloric content: ${largest}`);
+console.log(`highest caloric content: ${sorted[0]}`);
+
+console.log('----------------------------------------------------------------------');
+
+console.log('solution part 2)');
+console.log(`top 3 caloric sum: ${sorted.reduce((sum, calories) => sum + calories, 0)}`);
