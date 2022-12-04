732c06344342530e91bb116b5b658f6c41bf63c6
DP <dp@reproconnect.com>
2022-12-03 20:33:16 -0500

    day 2 part 2

--- 2022/day02/solution.ts
+++ 2022/day02/solution.ts
@@ -5,22 +5,48 @@ const rounds = (new TextDecoder('utf-8'))
             .map((hand) => (["A", "B", "C", "X", "Y", "Z"].indexOf(hand) % 3) + 1)
     );
 
-const results = rounds.map((round) => {
-    const result = [round[1]];
+function scoreForRound(round: number[]): number {
+    const choice = round[1];
+    let outcome: number;
     switch (Math.abs(round[1] - round[0])) {
         case 0: // Draw
-            result[1] = 3
+            outcome = 3
             break;
         case 1: // Higher wins
-            result[1] = (round.indexOf(Math.max(...round)) == 1) ? 6 : 0;
+            outcome = (round.indexOf(Math.max(...round)) == 1) ? 6 : 0;
             break;
         default: // Lower wins
-            result[1] = (round.indexOf(Math.min(...round)) == 1) ? 6 : 0;
+            outcome = (round.indexOf(Math.min(...round)) == 1) ? 6 : 0;
             break;
     }
 
-    return result;
-});
+    return choice + outcome;
+}
 
 console.log('solution part 1)');
-console.log(`total score: ${results.reduce((sum, resultSet) => sum + resultSet[0] + resultSet[1], 0)}`);
+console.log(`total score: ${rounds.map(scoreForRound).reduce((sum, result) => sum + result, 0)}`);
+
+console.log('----------------------------------------------------------------------');
+
+const otherResults = rounds.map((round) => {
+        const opponent = round[0] - 1 + 3; // Normalized to 0-2, then added 3 so negative results mod correctly.
+        let player = 0;
+
+        switch (round[1]) {
+            case 1: // Loss required
+                player = (opponent - 1) % 3;
+                break;
+            case 2: // Draw required
+                player = opponent % 3;
+                break;
+            case 3: // Win required
+                player = (opponent + 1) % 3;
+                break;
+        }
+
+        return [(opponent % 3) + 1, player + 1];
+    })
+    .map(scoreForRound);
+
+console.log('solution part 2)');
+console.log(`total score: ${otherResults.reduce((sum, result) => sum + result, 0)}`);
